825db198cff0e34c4457a1707935d314b47c3128
aniketcodes <aniketsinha1304@gmail.com>
2022-02-02 12:10:30 +0530

    hardhat installation and testing

---
+++ test/token.js
@@ -0,0 +1,53 @@
+const {expect}=require("chai");
+
+describe("Token contract",()=>{
+    let Token,token,owner,addr1,addr2;
+
+    beforeEach(async ()=>{
+        Token=await ethers.getContractFactory("Token");
+        token=await Token.deploy();
+        [owner,addr1,addr2,_]=await ethers.getSigners();
+    })
+
+    describe("Deployment",()=>{
+        it("Should set the right owner",async()=>{
+            expect(await token.owner()).to.equal(owner.address);
+        })
+        it("Should assign the total supply of tokens to te owner",async ()=>{
+            const ownerBalance=await token.balanceOf(owner.address);
+            expect(await token.totalSupply()).to.equal(ownerBalance);
+        })
+    })
+
+    describe("Transactions",()=>{
+        it("Should transfer tokens between accounts",async ()=>{
+            await token.transfer(addr1.address,50);
+            const addr1Balance=await token.balanceOf(addr1.address);
+            expect(addr1Balance).to.equal(50);
+
+            await token.connect(addr1).transfer(addr2.address,50);
+            const addr2Balance=await token.balanceOf(addr2.address);
+            expect(addr2Balance).to.equal(50);
+        });
+        it("Should fail if sender doesnt have enough tokens",async ()=>{
+            const initialBalanceOfOwner=await token.balanceOf(owner.address);
+            await expect(token.connect(addr1).transfer(owner.address,1)).to.be.revertedWith("Not enough tokens");
+
+            expect(await token.balanceOf(owner.address)).to.equal(initialBalanceOfOwner);
+
+        })
+        it("Should update balance after transfers",async()=>{
+            const initialBalanceOwner=await token.balanceOf(owner.address);
+            await token.transfer(addr1.address,100);
+            await token.transfer(addr2.address,50);
+
+            const finalOwnerBalance=await token.balanceOf(owner.address);
+            expect(finalOwnerBalance).to.equal(initialBalanceOwner-150);
+
+            const addr1Balance=await token.balanceOf(addr1.address);
+            expect(addr1Balance).to.equal(100);
+            const addr2Balance=await token.balanceOf(addr2.address);
+            expect(addr2Balance).to.equal(50);
+        })
+    })
+})
